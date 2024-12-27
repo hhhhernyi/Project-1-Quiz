e1c047afd719411ad37914521cb006b01cf51f09
Hern Yi Lee <hyileenet@gmail.com>
2024-12-28 01:30:05 +0800

    feat: added some html with styling and check that buttons work

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log('xxx');
+
+const button1 = document.querySelector("#answer1");
+button1.addEventListener("click", () => {
+  console.log("answer 1 is pressed!");
+});
+
+const button2 = document.querySelector("#answer2");
+button2.addEventListener("click", () => {
+  console.log("answer 2 is pressed!");
+});
+
+const button3 = document.querySelector("#answer3");
+button3.addEventListener("click", () => {
+  console.log("answer 3 is pressed!");
+});
+
+const button4 = document.querySelector("#answer4");
+button4.addEventListener("click", () => {
+  console.log("answer 4 is pressed!");
+});
+
+const quitButton = document.querySelector("#quitButton");
+quitButton.addEventListener("click", () => {
+    console.log("quit is pressed!");
+  });
